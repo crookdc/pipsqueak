361ded12a4f30ca717115f61b4934e7c961912af
crookdc <e.rasmus.nilsson@icloud.com>
2025-01-25 09:32:46 +0100

    Adds infix addition evaluation

--- src/eval.rs
+++ src/eval.rs
@@ -1,6 +1,10 @@
 use crate::lexer::Token;
 use crate::parser::ExpressionNode;
 use crate::parser::StatementNode;
+use std::any::type_name_of_val;
+use std::fmt::format;
+use std::mem;
+use std::ops::Add;
 
 #[derive(Debug, Eq, PartialEq)]
 pub enum Object {
@@ -24,6 +28,12 @@ impl EvalError {
     fn unexpected_operator(token: Token) -> EvalError {
         Self {
             msg: format!("unexpected operator {token:?}"),
+        }
+    }
+
+    fn mixed_operands(a: Object, b: Object) -> EvalError {
+        Self {
+            msg: format!("unexpected mixed operands {a:?} and {b:?}"),
         }
     }
 }
@@ -51,6 +61,24 @@ fn eval_expression(expr: ExpressionNode) -> Result<Object, EvalError> {
                     other => Err(EvalError::unexpected_operator(other)),
                 },
                 other => Err(EvalError::unexpected_type(other)),
+            }
+        }
+        ExpressionNode::Infix(operator, left, right) => {
+            let left = eval_expression(*left)?;
+            let right = eval_expression(*right)?;
+            match operator {
+                Token::Plus => {
+                    if let Object::Integer(a) = left {
+                        if let Object::Integer(b) = right {
+                            Ok(Object::Integer(a + b))
+                        } else {
+                            Err(EvalError::mixed_operands(left, right))
+                        }
+                    } else {
+                        Err(EvalError::unexpected_type(left))
+                    }
+                },
+                _ => todo!()
             }
         }
         _ => todo!(),
@@ -83,14 +111,36 @@ mod tests {
     fn test_prefix_expression_eval() {
         let assertions = vec![
             (
-                StatementNode::Expression(ExpressionNode::Prefix(Token::Minus, Box::new(ExpressionNode::Integer(10)))),
+                StatementNode::Expression(ExpressionNode::Prefix(
+                    Token::Minus,
+                    Box::new(ExpressionNode::Integer(10)),
+                )),
                 Object::Integer(-10),
             ),
             (
-                StatementNode::Expression(ExpressionNode::Prefix(Token::Bang, Box::new(ExpressionNode::Boolean(true)))),
+                StatementNode::Expression(ExpressionNode::Prefix(
+                    Token::Bang,
+                    Box::new(ExpressionNode::Boolean(true)),
+                )),
                 Object::Boolean(false),
             ),
         ];
+        for assert in assertions {
+            let actual = eval(assert.0).unwrap();
+            assert_eq!(assert.1, actual);
+        }
+    }
+
+    #[test]
+    fn test_infix_expression_eval() {
+        let assertions = vec![(
+            StatementNode::Expression(ExpressionNode::Infix(
+                Token::Plus,
+                Box::new(ExpressionNode::Integer(10)),
+                Box::new(ExpressionNode::Prefix(Token::Minus, Box::new(ExpressionNode::Integer(20)))),
+            )),
+            Object::Integer(-10),
+        )];
         for assert in assertions {
             let actual = eval(assert.0).unwrap();
             assert_eq!(assert.1, actual);
