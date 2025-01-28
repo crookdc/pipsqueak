8ad99d381ac25ebbfff29a04b5d668c4eff56d07
crookdc <e.rasmus.nilsson@icloud.com>
2025-01-28 18:49:54 +0100

    Processing and execution of script files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use crate::eval::Evaluator;
 use crate::lexer::Lexer;
+use crate::lexer::Token::LeftCurlyBrace;
 use crate::parser::Parser;
-use std::io;
 use std::io::{BufRead, Write};
+use std::{env, fs, io};
+use crate::builtin::print;
 
 mod builtin;
 mod eval;
@@ -9,10 +12,25 @@ mod lexer;
 mod parser;
 
 fn main() {
-    start_repl(&mut io::stdin().lock(), &mut io::stdout());
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        start_repl(&mut io::stdin().lock(), &mut io::stdout());
+    } else {
+        execute_script(args[1].as_str());
+    }
 }
 
-pub fn start_repl(input: &mut impl BufRead, output: &mut impl Write) {
+fn execute_script(path: &str) {
+    let src = fs::read_to_string(path).unwrap();
+    let mut parser = Parser::new(Lexer::new(src.chars().collect()));
+    let mut program = parser.parse().unwrap();
+    let mut eval = Evaluator::new();
+    while let Some(stmt) = program.pop() {
+        eval.eval(stmt).unwrap();
+    }
+}
+
+fn start_repl(input: &mut impl BufRead, output: &mut impl Write) {
     let mut evaluator = eval::Evaluator::new();
     loop {
         write!(output, ">>").unwrap();
